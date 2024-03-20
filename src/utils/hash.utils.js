2cb6604efad7740b8d4431944ba9307d16f078ea
Guido Zlatar <guido_zlatar07@hotmail.com>
2024-03-19 23:03:03 -0300

    entrega desafio

--- src/utils/hash.utils.js
+++
@@ -1,7 +0,0 @@
-import { genSaltSync, hashSync, compareSync } from "bcrypt";
-
-const createHash = (password) => hashSync(password, genSaltSync(10));
-
-const verifyHash = (req, db) => compareSync(req, db);
-
-export { createHash, verifyHash };
--- src/utils/isValidPass.utils.js
+++
@@ -1,9 +0,0 @@
-function isValidPass(formPassword, dbPassword) {
-    if (formPassword !== dbPassword) {
-      const error = new Error("Invalid credentials");
-      error.statusCode = 401;
-      throw error;
-    }
-  }
-  
-  export default isValidPass;
